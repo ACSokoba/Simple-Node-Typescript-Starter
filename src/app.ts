e0625beafd3f398fb00fcfbacd5772c0bc38462f
Aboubakar SOKOBA <aboubakar.sokoba@hotmail.com>
2020-01-06 00:25:16 +0100

    minor fixes

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 import lusca from "lusca";
 import flash from "express-flash";
 import path from "path";
-import passport from "passport";
 
 
 
@@ -17,24 +16,22 @@ const app = express();
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+
 app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
-app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
-});
-
-
 app.use(
     express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello world </h1>");
+});
+
+
 
 export default app;
